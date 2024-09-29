bd0c745343fb0a46cdfc026bde4e5e85154b1a71
Kevin Li <o0okevinli@gmail.com>
2024-09-29 17:58:59 -0400

    Made returnCopy method and fixed test cases by adding arrayToString to them

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -10,20 +10,23 @@ public class MyArrays {
     return result + "]";
   }
 
-/*  public static int[] returnCopy(int[] ary) {
-    String ogAry = arrayToString(ary);
+  public static int[] returnCopy(int[] ary) {
     int[] newAry = new int[ary.length];
-    for (int i = 1; i < ogAry.length(); i++) {
-      if (ogAry.charAt(i) != '[') {
-        newAry[i] = ogAry[i];
-      }
+    for (int i = 0; i < ary.length; i++) {
+      newAry[i] = ary[i];
     }
-    return newAry;
-  } */
+    //checks to make sure they are not the same array
+    if (ary != newAry) {
+      return newAry;
+    }
+    else {
+      return new int[] {-1};
+    }
+  }
 
-//  public static int[] concatArray(int[]ary1,int[]ary2) {
-//    return ary1;
-//  }
+  public static int[] concatArray(int[]ary1,int[]ary2) {
+    return ary1;
+  }
 
   public static void main(String[] args) {
     // arrayToString test cases
@@ -31,19 +34,19 @@ public class MyArrays {
     System.out.println("Expected []. Returned: " + arrayToString(new int[] {}));
 
     //returnCopy test cases
-    System.out.println("Expected [1, 2, 3, 4]. Returned: " + returnCopy(new int[] {1, 2, 3, 4}));
-    System.out.println("Expected [10, 20, 30, 40]. Returned: " + returnCopy(new int[] {10, 20, 30, 40}));
-    System.out.println("Expected [-10, -20, 30, 40, -50]. Returned: " + returnCopy(new int[] {-10, -20, 30, 40, -50}));
-    System.out.println("Expected [0, -27, 15, -40, 0]. Returned: " + returnCopy(new int[] {0, -27, 15, -40, 0}));
-    System.out.println("Expected [0]. Returned: " + returnCopy(new int[] {0}));
-    System.out.println("Expected [-123456]. Returned: " + returnCopy(new int[] {-123456}));
+    System.out.println("Expected [1, 2, 3, 4]. Returned: " + arrayToString(returnCopy(new int[] {1, 2, 3, 4})));
+    System.out.println("Expected [10, 20, 30, 40]. Returned: " + arrayToString(returnCopy(new int[] {10, 20, 30, 40})));
+    System.out.println("Expected [-10, -20, 30, 40, -50]. Returned: " + arrayToString(returnCopy(new int[] {-10, -20, 30, 40, -50})));
+    System.out.println("Expected [0, -27, 15, -40, 0]. Returned: " + arrayToString(returnCopy(new int[] {0, -27, 15, -40, 0})));
+    System.out.println("Expected [0]. Returned: " + arrayToString(returnCopy(new int[] {0})));
+    System.out.println("Expected [-123456]. Returned: " + arrayToString(returnCopy(new int[] {-123456})));
 
     //concatArray test cases
-    System.out.println("Expected [-12, 13, -14, 15, -16, 17]. Returned: " + concatArray(new int[] {-12, 13, -14}, new int[] {15, -16 17}));
-    System.out.println("Expected [0, 0]. Returned: " + concatArray(new int[] {0}, new int[] {0}));
-    System.out.println("Expected [1, 2, 3, 4]. Returned: " + concatArray(new int[] {1, 2, 3} new int[] {4}));
-    System.out.println("Expected [1, 2, 3, 4]. Returned: " + concatArray(new int[] {1}, new int[] {2, 3, 4}));
-    System.out.println("Expected [123456, -123456]. Returned: " + concatArray(new int[] {123456}, new int[] {-123456}));
-    System.out.println("Expected [-2000, 2000, -5000]. Returned: " + concatArray(new int[] {-2000, 2000}, new int[] {-5000}));
+    System.out.println("Expected [-12, 13, -14, 15, -16, 17]. Returned: " + arrayToString(concatArray(new int[] {-12, 13, -14}, new int[] {15, -16, 17})));
+    System.out.println("Expected [0, 0]. Returned: " + arrayToString(concatArray(new int[] {0}, new int[] {0})));
+    System.out.println("Expected [1, 2, 3, 4]. Returned: " + arrayToString(concatArray(new int[] {1, 2, 3}, new int[] {4})));
+    System.out.println("Expected [1, 2, 3, 4]. Returned: " + arrayToString(concatArray(new int[] {1}, new int[] {2, 3, 4})));
+    System.out.println("Expected [123456, -123456]. Returned: " + arrayToString(concatArray(new int[] {123456}, new int[] {-123456})));
+    System.out.println("Expected [-2000, 2000, -5000]. Returned: " + arrayToString(concatArray(new int[] {-2000, 2000}, new int[] {-5000})));
   }
 }
