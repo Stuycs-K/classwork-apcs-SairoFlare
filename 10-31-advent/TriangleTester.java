86a5b49b1094b372411c4eeced3bed27739b43b2
Kevin Li <o0okevinli@gmail.com>
2024-10-31 10:59:15 -0500

    feat: added TrianglecountB

--- 10-31-advent/TriangleTester.java
+++ 10-31-advent/TriangleTester.java
@@ -34,4 +34,39 @@ public class TriangleTester
       return 0;
     }
   }
+  public static int countTrianglesB(String filename)
+  {
+    try{
+      File name = new File(filename);
+      Scanner file = new Scanner(name);
+      int count = 0;
+      while(file.hasNextLine())
+        {
+        int[] t0 = new int[3];
+        int[] t1 = new int[3];
+        int[] t2 = new int[3];
+        for (int k = 0; k < 3; k++)
+          {
+            t0[k] = Integer.parseInt(file.next());
+            t1[k] = Integer.parseInt(file.next());
+            t2[k] = Integer.parseInt(file.next());
+            file.nextLine();
+          }
+        count += (isValid(t0[0],t0[1],t0[2]))? 1 :0;
+        count += (isValid(t1[0],t1[1],t1[2]))? 1 :0;
+        count += (isValid(t2[0],t2[1],t2[2]))? 1 :0;
+        }
+        return count;
+      }
+      catch (FileNotFoundException e)
+      {
+        System.out.println("lol");
+        return 0;
+      }
+
+    }
+    private static boolean isValid(int v0, int v1, int v2)
+    {
+      return (v0 + v1 > v2 && v0 + v2 > v1 && v1 + v2 > v0);
+    }
 }
