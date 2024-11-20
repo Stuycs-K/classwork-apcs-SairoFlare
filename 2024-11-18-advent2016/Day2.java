1c14d93ec6fd16b1f9fd9a5e945db9a9455c3404
Kevin Li <o0okevinli@gmail.com>
2024-11-19 22:01:35 -0500

    Made keyPress helper method

--- 2024-11-18-advent2016/Day2.java
+++ 2024-11-18-advent2016/Day2.java
@@ -9,30 +9,33 @@ public class Day2 {
       File name = new File(filename);
       Scanner file = new Scanner(name);
       ArrayList<Integer> result = new ArrayList<Integer>(3);
-      int x = 0;
-      int y = 0;
+      int x = 1;
+      int y = 1;
       while (file.hasNextLine()) {
         String num = file.nextLine();
         //System.out.println(num);
         for (int i = 0; i < num.length(); i++) {
           String temp = num.substring(i, i + 1);
-          if (temp.equals("U") && y <= 0) {
-            y += 1;
-            System.out.println(y + "y");
-          }
-          else if (temp.equals("D") && y >= 0) {
+          if (temp.equals("U") && y >= 1) {
             y -= 1;
-            System.out.println(y + "y");
+            //System.out.println(y + "y");
           }
-          else if (temp.equals("R") && x <= 0) {
+          else if (temp.equals("D") && y <= 1) {
+            y += 1;
+            //System.out.println(y + "y");
+          }
+          else if (temp.equals("R") && x <= 1) {
             x += 1;
-            System.out.println(x + "x");
+            //System.out.println(x + "x");
           }
-          else if (temp.equals("L") && x >= 0) {
+          else if (temp.equals("L") && x >= 1) {
             x -= 1;
-            System.out.println(x + "x");
+            //System.out.println(x + "x");
           }
         }
+        //System.out.println(x + "x");
+        //System.out.println(y + "y");
+        result.add(keyPress(x, y));
       }
       return result;
     }
@@ -40,5 +43,10 @@ public class Day2 {
       System.out.println("file not found");
       return new ArrayList<Integer>(1);
     }
+  }
+
+  public static int keyPress(int x, int y) {
+    int[][] numpad = new int[][]{{1,2,3}, {4,5,6}, {7,8,9}};
+    return numpad[x][y];
   }
 }
