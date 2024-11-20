64d9824b25a762000b8e2a967965b624211eab55
Kevin Li <o0okevinli@gmail.com>
2024-11-20 11:40:46 -0500

    Changed file name to day4 and not day 3

---
+++ 2024-11-18-advent2016/Day4.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Day4 {
+  public static int solver(String filename) {
+    try {
+      File name = new File(filename);
+      Scanner file = new Scanner(name);
+      int result = 0;
+      while (file.hasNextLine()) {
+
+      }
+      return result;
+    }
+    catch (FileNotFoundException e) {
+      System.out.println("file not found");
+      return 0;
+    }
+  }
+}
