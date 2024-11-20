9b23b28dd546bcff40d4f68659e04f49ad4507b0
Kevin Li <o0okevinli@gmail.com>
2024-11-19 21:18:15 -0500

    Made code to loop through each line of the input

--- 2024-11-18-advent2016/Day2.java
+++ 2024-11-18-advent2016/Day2.java
@@ -4,14 +4,22 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Day2 {
-  public static ArrayList<Integer>solver (ArrayList<String> input) {
+  public static ArrayList<Integer> keypadReader(String filename) {
     try {
       File name = new File(filename);
       Scanner file = new Scanner(name);
+      ArrayList<Integer> result = new ArrayList<Integer>(3);
+      int x = 0;
+      int y = 0;
+      while (file.hasNextLine()) {
+        String num = file.nextLine();
+        System.out.println(num);
+      }
+      return result;
     }
     catch (FileNotFoundException e) {
       System.out.println("file not found");
-      return new ArrayList(1);
+      return new ArrayList<Integer>(1);
     }
   }
 }
--- 2024-11-18-advent2016/Driver.java
+++ 2024-11-18-advent2016/Driver.java
@@ -1,5 +1,5 @@
 public class Driver {
   public static void main(String[] args) {
-    System.out.println(Blocks.countBlocks("input.txt"));
+    System.out.println(Day2.keypadReader("input.txt"));
   }
 }
