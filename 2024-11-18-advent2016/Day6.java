0aa37d283966d264f39703c9b543950c45996e59
Kevin Li <o0okevinli@gmail.com>
2024-11-23 17:34:23 -0500

    Made new loop to loop through each column and made a helper method to count letters in a string

--- 2024-11-18-advent2016/Day6.java
+++ 2024-11-18-advent2016/Day6.java
@@ -8,14 +8,24 @@ public class Day6 {
       File name = new File(filename);
       Scanner file = new Scanner(name);
       String result = "";
-      ArrayList<String> fails = new ArrayList<String>();
+      //ArrayList<String> fails = new ArrayList<String>();
       String[] columns = new String[]{"","","","","","","",""};
       while (file.hasNextLine()) {
         String line = file.nextLine();
         for (int i = 0; i < 8; i++) {
           columns[i] = columns[i] + line.substring(i, i + 1);
-          System.out.println(columns[i]);
+          //System.out.println(columns[i]);
         }
+      }
+      for (int i = 0; i < 8; i++) {
+        String temp = "";
+        int greatest = 0;
+        for (int index = 0; index < columns[i].length(); index++) {
+          //System.out.println(columns[i].length());
+          String letter = (columns[i]).substring(index, index + 1);
+          //System.out.println(letter);
+        }
+        //result = result + letter;
       }
       return result;
     }
@@ -23,5 +33,15 @@ public class Day6 {
       System.out.println("file not found");
       return null;
     }
+  }
+
+  public static int count(String letter, String column) {
+    int result = 0;
+    for (int i = 0; i < column.length(); i++) {
+      if (letter.equals(column.substring(i, i + 1))) {
+        result++;
+      }
+    }
+    return result;
   }
 }
