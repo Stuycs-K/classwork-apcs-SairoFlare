c44f18867badce77ca98be3df2ce117f61890469
Kevin Li <o0okevinli@gmail.com>
2024-11-23 18:18:37 -0500

    Made a helper method to check if a block has been moved over twice

--- 2024-11-18-advent2016/Blocks.java
+++ 2024-11-18-advent2016/Blocks.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Blocks {
   public static int countBlocks(String filename) {
@@ -10,26 +11,29 @@ public class Blocks {
       int horizontal = 0;
       int vertical = 0;
       int direction = 0;
+      ArrayList<int[]> movements = new ArrayList<int[]>();
+      movements.add(new int[]{0,0});
       while (file.hasNext()) {
         String movement = file.next();
         direction += turn(movement);
         direction = (direction + 4) % 4;
         if (direction == 0) {
-          vertical += magnitude(movement);
+          //vertical += magnitude(movement);
           //System.out.println(vertical + "vert");
         }
         else if (direction == 1) {
-          horizontal += magnitude(movement);
+          //horizontal += magnitude(movement);
           //System.out.println(horizontal + "hori");
         }
         else if (direction == 2) {
-          vertical -= magnitude(movement);
+          //vertical -= magnitude(movement);
           //System.out.println(vertical + "vert");
         }
         else {
-          horizontal -= magnitude(movement);
+          //horizontal -= magnitude(movement);
           //System.out.println(horizontal + "hori");
         }
+        movements.add(new int[]{horizontal, vertical});
       }
       return Math.abs(horizontal) + Math.abs(vertical);
     }
@@ -37,6 +41,19 @@ public class Blocks {
       System.out.println("file not found");
       return -1;
     }
+  }
+
+  public static int twiceCheck(ArrayList<int[]> movements, int horizontal, int vertical) {
+    for (int i = 0; i < movements.size(); i++) {
+      //System.out.println(horizontal + ", " + vertical);
+      //System.out.println(movements.get(i)[0]);
+      //System.out.println(movements.get(i)[1]);
+      if (movements.get(i)[0] == horizontal && movements.get(i)[1] == vertical) {
+        //System.out.println(horizontal + ", " + vertical);
+        return Math.abs(horizontal) + Math.abs(vertical);
+      }
+    }
+    return -1;
   }
 
   public static int turn(String movement) {
