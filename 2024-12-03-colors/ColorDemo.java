eac5fe714d95c0e8b2c344961684960c2106a305
Kevin Li <o0okevinli@gmail.com>
2024-12-04 00:02:01 -0500

    Added first row of TKI opener

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -6,23 +6,26 @@ public class ColorDemo {
   public static void colorBack(int r, int g, int b){
     System.out.print("\u001b[48;2" + r + ";" + g + ";" + b + "m");
   }*/
-  // cant figure out why rgb doesnt work
   public static void color(int foreground, int background){
     System.out.print("\u001b[" + foreground + ";" + (background + 10) + "m");
   }
 
   public static void main(String[] args) {
     //colorBack(10, 200, 255);
-    System.out.println("\u001b[38;2;255;0;255mPurple");
+    //System.out.println("\u001b[38;2;255;0;255mPurple");
     //tries to make T piece from tetris
-    color(35, 35);
-    System.out.print("---");
-    System.out.println("\u001b[0m");
+    System.out.print("\u001b[38;2;255;165;0m+");
     color(30, 30);
-    System.out.print("-");
     color(35, 35);
-    System.out.println("-");
-    System.out.print("\u001b[0m");
+    System.out.print("--");
+    color(31, 31);
+    System.out.print("+");
+    System.out.println("\u001b[0m");
+    //color(30, 30);
+    //System.out.print("-");
+    //color(35, 35);
+    //System.out.println("+");
+    //System.out.print("\u001b[0m");
   }
 
 }
