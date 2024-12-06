952c47ca3fc4551704b6b0b77413d3939efe4499
Kevin Li <o0okevinli@gmail.com>
2024-12-06 11:41:00 -0500

    Made accessor methods, fixed constructors, and tested

---
+++ 2024-12-06-Adventurer/Driver.java
@@ -0,0 +1,18 @@
+public class Driver {
+  public static void main(String[] args) {
+    Adventurer p1 = new Rogue("Maya", 8);
+    System.out.println(p1.getSpecialName());
+    System.out.println(p1.getSpecial());
+    System.out.println(p1.getSpecialMax());
+    p1.setSpecial(3);
+    System.out.println(p1.getSpecial());
+    System.out.println(p1.getSpecialMax());
+    Adventurer p2 = new Rogue("Paul");
+    System.out.println(p2.getSpecialName());
+    System.out.println(p2.getSpecial());
+    System.out.println(p2.getSpecialMax());
+    p2.setSpecial(3);
+    System.out.println(p2.getSpecial());
+    System.out.println(p2.getSpecialMax());
+  }
+}
--- 2024-12-06-Adventurer/Rogue.java
+++ 2024-12-06-Adventurer/Rogue.java
@@ -1,26 +1,32 @@
 public class Rogue extends Adventurer {
+  private int sneak, maxSneak;
+
 
   public Rogue(String name){
-      super(name, 10);
+      super(name);
+      this.sneak = 5;
+      this.maxSneak = 5;
   }
 
   public Rogue(String name, int hp){
     super(name, hp);
+    this.sneak = 5;
+    this.maxSneak = 5;
   }
 
   //give it a short name (fewer than 13 characters)
   public String getSpecialName() {
-
+    return "sneak";
   }
   //accessor methods
   public int getSpecial() {
-
+    return sneak;
   }
   public void setSpecial(int n) {
-
+    sneak = n;
   }
   public int getSpecialMax() {
-
+    return maxSneak;
   }
 
   /*
@@ -29,21 +35,21 @@ public class Rogue extends Adventurer {
   */
   //hurt or hinder the target adventurer
   public String attack(Adventurer other) {
-
+    return "";
   }
 
   //heall or buff the target adventurer
   public String support(Adventurer other) {
-
+    return "";
   }
 
   //heall or buff self
   public String support() {
-
+    return "";
   }
 
   //hurt or hinder the target adventurer, consume some special resource
   public String specialAttack(Adventurer other) {
-
+    return "";
   }
 }
