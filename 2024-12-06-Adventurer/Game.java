2c174e6db932cab8a686143ebeb33ca33e0f3de5
Kevin Li <o0okevinli@gmail.com>
2024-12-09 21:17:10 -0500

    Fixed code to be able to take user input and made quit and attack options

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -4,13 +4,24 @@ public class Game {
     boolean exit = false;
     Adventurer player = new Rogue("Player", 12);
     Adventurer enemy = new CodeWarrior("Mr. Man", 10, "java");
+    Scanner userInput = new Scanner(System.in);
     //Scanner userInput = new Scanner(System.in);
     while (player.getHP() > 0 && enemy.getHP() > 0 && !exit) {
       System.out.println("Player, " + player.getHP() + "/" + player.getmaxHP() + "HP, " + player.getSpecial() + "/" + player.getSpecialMax() + " " + player.getSpecialName());
       System.out.println("Mr. Man, " + enemy.getHP() + "/" + enemy.getmaxHP() + "HP, " + enemy.getSpecial() + "/" + enemy.getSpecialMax() + " " + enemy.getSpecialName());
       System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
-      Scanner userInput = new Scanner(System.in);
-      exit = true;
+      String userChoice = userInput.nextLine();
+      while (!(userChoice.equals("a") || userChoice.equals("attack") || userChoice.equals("sp") || userChoice.equals("special") || userChoice.equals("su") || userChoice.equals("support") || userChoice.equals("quit"))) {
+        System.out.println("Invalid input, try again.");
+        System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+        userChoice = userInput.nextLine();
+      }
+      if (userChoice.equals("quit")) {
+        exit = true;
+      }
+      else if (userChoice.equals("a") || userChoice.equals("attack")) {
+        System.out.println(player.attack(enemy));
+      }
     }
   }
 }
